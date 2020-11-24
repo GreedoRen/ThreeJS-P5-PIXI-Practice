9cc00d42fc0f55fb2c123936534f8a0a4c332e62
GreedoRen <greedo.it@gmail.com>
2020-11-24 21:31:57 +0300

    draw

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const fish = document.getElementById('fish_colored')
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-ctx.drawImage(image, dx, dy, dWidth, dHeight)
-ctx.getImageData(sx, sy, sw, sh)
+ctx.drawImage(fish, 0, 0, fish.width, fish.hight)
+ctx.getImageData(0, 0, fish.width, fish.hight)
 
