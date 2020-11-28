6d781121657cc84f0e27a4b2b692e232ae313aeb
GreedoRen <greedo.it@gmail.com>
2020-11-28 23:05:11 +0300

    data

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 ctx.drawImage(fish, 0, 0, fish.width, fish.hight)
-ctx.getImageData(0, 0, fish.width, fish.hight)
+let data = ctx.getImageData(0, 0, fish.width, fish.hight)
+
+ctx.putImageData(data, 0, 0)
 
