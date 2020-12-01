53dab1ccd9de4f4822e8fbc4a5e0f72902f98644
GreedoRen <greedo.it@gmail.com>
2020-12-01 22:17:11 +0300

    console

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ ctx.drawImage(fish, 0, 0, fish.width, fish.hight)
 let data = ctx.getImageData(0, 0, fish.width, fish.hight)
 ctx.putImageData(data, 0, 0)
 
+console.log(data)
+
 ctx.drawImage(fishGrayscale, 0, 0, fishGrayscale.width, fishGrayscale.hight)
 let grayData = ctx.getImageData(0, 0, fishGrayscale.width, fishGrayscale.hight)
 ctx.putImageData(grayData, 0, 0)
+
+console.log(grayData)
 
